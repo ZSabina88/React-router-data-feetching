5ffdea079b456622db99e30721e49cec1cfe42f5
Sabina Zeynalova <117943057+ZSabina88@users.noreply.github.com>
2024-02-04 01:09:34 +0400

    initial

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
+export default function EventsPage() {
+  const events = useLoaderData();
+  return (
+    <EventsList events={events} />
+  );
+};
+
+export async function eventsLoader() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
